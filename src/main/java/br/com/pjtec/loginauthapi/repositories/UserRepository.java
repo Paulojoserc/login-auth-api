85fab275f15c8067e6c9d624df978b4c19ce59e0
Paulojoserc <pjrc83@gmail.com>
2024-04-01 09:40:45 -0300

    Criacao SercurityFilter, UserRepository e CustomUserDetailsService

--- src/main/java/br/com/pjtec/loginauthapi/repositories/UserRepository.java
+++ src/main/java/br/com/pjtec/loginauthapi/repositories/UserRepository.java
@@ -3,5 +3,8 @@ package br.com.pjtec.loginauthapi.repositories;
 import br.com.pjtec.loginauthapi.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
 }
